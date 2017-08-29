3089f7a47e1f21db8ac1a08ac3bc97b5fd7dc1b3
Karthik Balakrishnan <karthikb351@gmail.com>
2017-08-29 18:50:15 +0530

    Push front end updates

---
+++ deploy/build.js
@@ -0,0 +1,35 @@
+var Tabletop = require('tabletop');
+var jsonfile = require('jsonfile');
+var async = require('async');
+
+
+var sheets = [
+  {
+    name: "Contacts",
+    url: 'https://docs.google.com/spreadsheets/d/1lNF2JFoDR3uQ8-dDQHUXvD_7ZYDyCFyRwLNsrzoZwIs/edit?usp=sharing',
+    api: 'updates.json',
+    sheetName: "#RainHosts",
+  }
+];
+
+async.forEach(sheets, function (item, callback){
+
+  function writeData(dataSet, tabletop) {
+    var obj = {
+      columns: tabletop.sheets(item.sheetName)['column_names'],
+      rows: tabletop.sheets(item.sheetName).toArray()
+    }
+    var file = 'api/'+item.api;
+
+    jsonfile.writeFileSync(file, obj)
+    console.log('Saved: '+item.name);
+    callback();
+  }
+  Tabletop.init( { key: item.url,
+    callback: writeData,
+    wanted: [item.sheetName],
+    simpleSheet: false } )
+
+  }, function(err) {
+    console.log('Done');
+  });
